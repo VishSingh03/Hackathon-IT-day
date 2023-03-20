6e06b6ac8d32250b4b3079d8506ef1922d11b67f
VishSingh03 <97512970+VishSingh03@users.noreply.github.com>
2023-03-20 13:57:53 +0530

    Add files via upload

---
+++ Farming Tools and Services/index.js
@@ -0,0 +1,17 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick =() =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+function loader() {
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut() {
+    setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
